ed30cde04f60925a1d28e4cda4b0a059ad6a0241
Taher Abouzeid <tabouzeid@gmail.com>
2020-07-25 11:49:56 -0400

    adding routes to retrieve html pages

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+
+require("./routes/htmlRoutes")(app);
+
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
